92536c4aee23993929e1a13f52bacce1c20103b3
Jamie <jamie.mcgurk@contrastsecurity.com>
2022-04-06 18:02:01 +0100

    v4

--- plumbingTests.rb
+++ plumbingTests.rb
@@ -5,13 +5,13 @@ class Plumbingtests < Formula
   license "MIT"
 
   if OS.mac?
-    url "https://github.com/qwerty797/plumbingTests/releases/download/v0.0.3/plumbingTests-macos"
-    sha256 "9e048091d9c62ebda02c8909c92d879c71cc44050652f9137fcab91c3708f349"
+    url "https://github.com/qwerty797/plumbingTests/releases/download/v0.0.4/plumbingTests-macos"
+    sha256 "b5c467cde50d1d50f55ede339f2c5cee539e041f2fdffaaa61e953758fee0936"
     
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/qwerty797/plumbingTests/releases/download/v0.0.3/plumbingTests-macos"
-      sha256 "9e048091d9c62ebda02c8909c92d879c71cc44050652f9137fcab91c3708f349"
+      url "https://github.com/qwerty797/plumbingTests/releases/download/v0.0.4/plumbingTests-macos"
+      sha256 "b5c467cde50d1d50f55ede339f2c5cee539e041f2fdffaaa61e953758fee0936"
     end
   end
 
